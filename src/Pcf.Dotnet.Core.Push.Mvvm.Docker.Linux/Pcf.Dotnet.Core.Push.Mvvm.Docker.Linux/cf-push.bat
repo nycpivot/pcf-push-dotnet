11787a74490ea3ef6b1da1a721b0e1a4f5dfb178
developer <developer@thejameshome.net>
2019-05-09 21:36:20 -0400

    Renamed docker projects.

--- src/Pcf.Dotnet.Core.Push.Mvvm.Docker.Linux/Pcf.Dotnet.Core.Push.Mvvm.Docker.Linux/cf-push.bat
+++
@@ -1,8 +0,0 @@
-cf target -s pcf-dotnet-push
-pause
-
-cf delete pcf-dotnet-core-push-mvvm-docker-linux -r -f
-pause
-
-cf push pcf-dotnet-core-push-mvvm-docker-linux --docker-image nycpivot/pcf-dotnet-core-push-mvvm-docker-linux
-pause
